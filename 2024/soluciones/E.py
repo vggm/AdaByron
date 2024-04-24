1ddcd5e5901a0e09c1dc814cd8cb6a79725d9e1c
Victor Gonzalez Morales <victorgm2709@gmail.com>
2024-04-24 15:14:17 +0200

    E Solved

--- 2024/soluciones/E.py
+++ 2024/soluciones/E.py
@@ -1,5 +1,5 @@
 
-from collections import deque, defaultdict
+from collections import deque
 
 
 Neighbours = dict[int, list[int]]
@@ -26,15 +26,18 @@ def get_distances(neighbours: Neighbours, src: int) -> list[int]:
 def resolve(neighbours: Neighbours, alt: ALT):
   total_nodes = len(neighbours)
   A, L, T = alt
-  A_distance = get_distances(neighbours, A)
-  # print('A:', A_distance)
-  L_distance = get_distances(neighbours, L)
-  # print('L:',L_distance)
-  T_distance = get_distances(neighbours, T)
-  # print('T:',T_distance)
   
+  A_distance = get_distances(neighbours, A)
+  L_distance = get_distances(neighbours, L)
+  T_distance = get_distances(neighbours, T)
+  
+  result = float('inf')
   for node in range(1, total_nodes + 1):
-    pass
+    s = A_distance[node] + L_distance[node] + T_distance[node]
+    if s < result:
+      result = s
+  
+  return result
 
 
 if __name__ == '__main__':
@@ -47,7 +50,7 @@ if __name__ == '__main__':
       
       nodes, edges, A, L, T = list(map(int, entrada.split()))
       
-      neighbours = defaultdict(list)
+      neighbours = [[] for _ in range(nodes + 1)]
       
       for _ in range(edges):
         src, dst = list(map(int, input().split()))
